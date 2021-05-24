5e26dc6d8505d39fea38aa0a463cdc9dd78bb023
nvCuong <nguyenvancuong215@gmail.com>
2021-05-24 21:03:35 +0700

    lesson7

--- ONL-webadvance-DuongBen/lesson6_dom/script.js
+++ ONL-webadvance-DuongBen/lesson6_dom/script.js
@@ -1,72 +1,185 @@
-let data = [
+let Duong = [
     {
-        img: "https://cdn.cellphones.com.vn/media/catalog/product/cache/7/image/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone-12-pro-max_1__7.jpg",
-        name: "iPhone 12 Pro Max",
-        des:"Cứ mỗi năm, đến dạo cuối tháng 8 và gần đầu tháng 9 thì mọi thông tin sôi sục mới về chiếc iPhone mới lại xuất hiện. Apple năm nay lại ra thêm một chiếc iPhone mới với tên gọi mới là iPhone 12 Pro Max, đây là một dòng điện thoại mới và mạnh mẽ nhất của nhà Apple năm nay. Mời bạn tham khảo thêm một số mô tả sản phẩm bên dưới để bạn có thể ra quyết định mua sắm.",
-    },
-    {
-        img: "https://cdn.cellphones.com.vn/media/catalog/product/cache/7/image/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone-12-pro-max_3__9.jpg",
-        name: "iPhone 12 Pro ",
-        des:"Helllo",
-    },
-    {
-        img: "https://cdn.cellphones.com.vn/media/catalog/product/cache/7/image/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone-12-pro-max_1__7.jpg",
-        name: "iPhone 12 Pro Max",
-        des:"Cứ mỗi năm, đến dạo cuối tháng 8 và gần đầu tháng 9 thì mọi thông tin sôi sục mới về chiếc iPhone mới lại xuất hiện. Apple năm nay lại ra thêm một chiếc iPhone mới với tên gọi mới là iPhone 12 Pro Max, đây là một dòng điện thoại mới và mạnh mẽ nhất của nhà Apple năm nay. Mời bạn tham khảo thêm một số mô tả sản phẩm bên dưới để bạn có thể ra quyết định mua sắm.",
-    },
-    {
-        img: "https://cdn.cellphones.com.vn/media/catalog/product/cache/7/image/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone-12-pro-max_1__7.jpg",
-        name: "iPhone 12 Pro Max",
-        des:"Cứ mỗi năm, đến dạo cuối tháng 8 và gần đầu tháng 9 thì mọi thông tin sôi sục mới về chiếc iPhone mới lại xuất hiện. Apple năm nay lại ra thêm một chiếc iPhone mới với tên gọi mới là iPhone 12 Pro Max, đây là một dòng điện thoại mới và mạnh mẽ nhất của nhà Apple năm nay. Mời bạn tham khảo thêm một số mô tả sản phẩm bên dưới để bạn có thể ra quyết định mua sắm.",
-    },
-    {
-        img: "https://cdn.cellphones.com.vn/media/catalog/product/cache/7/image/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone-12-pro-max_1__7.jpg",
-        name: "iPhone 12 Pro Max",
-        des:"Cứ mỗi năm, đến dạo cuối tháng 8 và gần đầu tháng 9 thì mọi thông tin sôi sục mới về chiếc iPhone mới lại xuất hiện. Apple năm nay lại ra thêm một chiếc iPhone mới với tên gọi mới là iPhone 12 Pro Max, đây là một dòng điện thoại mới và mạnh mẽ nhất của nhà Apple năm nay. Mời bạn tham khảo thêm một số mô tả sản phẩm bên dưới để bạn có thể ra quyết định mua sắm.",
-    },
-    {
-        img: "https://cdn.cellphones.com.vn/media/catalog/product/cache/7/image/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone-12-pro-max_1__7.jpg",
-        name: "iPhone 12 Pro Max",
-        des:"Cứ mỗi năm, đến dạo cuối tháng 8 và gần đầu tháng 9 thì mọi thông tin sôi sục mới về chiếc iPhone mới lại xuất hiện. Apple năm nay lại ra thêm một chiếc iPhone mới với tên gọi mới là iPhone 12 Pro Max, đây là một dòng điện thoại mới và mạnh mẽ nhất của nhà Apple năm nay. Mời bạn tham khảo thêm một số mô tả sản phẩm bên dưới để bạn có thể ra quyết định mua sắm.",
-    },
-    {
-        img: "https://cdn.cellphones.com.vn/media/catalog/product/cache/7/image/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone-12-pro-max_1__7.jpg",
-        name: "iPhone 12 Pro Max",
-        des:"Cứ mỗi năm, đến dạo cuối tháng 8 và gần đầu tháng 9 thì mọi thông tin sôi sục mới về chiếc iPhone mới lại xuất hiện. Apple năm nay lại ra thêm một chiếc iPhone mới với tên gọi mới là iPhone 12 Pro Max, đây là một dòng điện thoại mới và mạnh mẽ nhất của nhà Apple năm nay. Mời bạn tham khảo thêm một số mô tả sản phẩm bên dưới để bạn có thể ra quyết định mua sắm.",
-    },
-    {
-        img: "https://cdn.cellphones.com.vn/media/catalog/product/cache/7/image/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone-12-pro-max_1__7.jpg",
-        name: "iPhone 12 Pro Max",
-        des:"Cứ mỗi năm, đến dạo cuối tháng 8 và gần đầu tháng 9 thì mọi thông tin sôi sục mới về chiếc iPhone mới lại xuất hiện. Apple năm nay lại ra thêm một chiếc iPhone mới với tên gọi mới là iPhone 12 Pro Max, đây là một dòng điện thoại mới và mạnh mẽ nhất của nhà Apple năm nay. Mời bạn tham khảo thêm một số mô tả sản phẩm bên dưới để bạn có thể ra quyết định mua sắm.",
-    },
-    {
-        img: "https://cdn.cellphones.com.vn/media/catalog/product/cache/7/image/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone-12-pro-max_1__7.jpg",
-        name: "iPhone 12 Pro Max",
-        des:"Cứ mỗi năm, đến dạo cuối tháng 8 và gần đầu tháng 9 thì mọi thông tin sôi sục mới về chiếc iPhone mới lại xuất hiện. Apple năm nay lại ra thêm một chiếc iPhone mới với tên gọi mới là iPhone 12 Pro Max, đây là một dòng điện thoại mới và mạnh mẽ nhất của nhà Apple năm nay. Mời bạn tham khảo thêm một số mô tả sản phẩm bên dưới để bạn có thể ra quyết định mua sắm.",
-    },
-    {
-        img: "https://cdn.cellphones.com.vn/media/catalog/product/cache/7/image/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone-12-pro-max_1__7.jpg",
-        name: "Nguyeexn Duc Ben",
-        des:"Cứ mỗi năm, đến dạo cuối tháng 8 và gần đầu tháng 9 thì mọi thông tin sôi sục mới về chiếc iPhone mới lại xuất hiện. Apple năm nay lại ra thêm một chiếc iPhone mới với tên gọi mới là iPhone 12 Pro Max, đây là một dòng điện thoại mới và mạnh mẽ nhất của nhà Apple năm nay. Mời bạn tham khảo thêm một số mô tả sản phẩm bên dưới để bạn có thể ra quyết định mua sắm.",
-    },
-    {
-        img: "https://cdn.cellphones.com.vn/media/catalog/product/cache/7/image/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone-12-pro-max_1__7.jpg",
-        name: "iPhone 12 Pro Max",
-        des:"Cứ mỗi năm, đến dạo cuối tháng 8 và gần đầu tháng 9 thì mọi thông tin sôi sục mới về chiếc iPhone mới lại xuất hiện. Apple năm nay lại ra thêm một chiếc iPhone mới với tên gọi mới là iPhone 12 Pro Max, đây là một dòng điện thoại mới và mạnh mẽ nhất của nhà Apple năm nay. Mời bạn tham khảo thêm một số mô tả sản phẩm bên dưới để bạn có thể ra quyết định mua sắm.",
+        name:"Ultimate PS5 Bundle",
+        img:"https://cdn.shopify.com/s/files/1/0490/7312/7591/products/gaming-shop-946837_500x.jpg?v=1611086850",
+        des:"Discover its lightning-fast loading times with an ultra-high speed SSD, deeper immersion with support for haptic feedback, adaptive triggers and 3D Audio. The new generation of gaming also comes with 4K, 8K output, and as usual; an all-new generation of incredible PlayStation Games. Your games graphics have never been as stunning!"
+    },    {
+        name:"Nintendo Switch Console with Neon Red/Blue Joy-Con",
+        img:"https://cdn.shopify.com/s/files/1/0490/7312/7591/products/gaming-shop-canada-278274_500x.jpg?v=1602274843",
+        des:"With up to 9 hours of battery, do what you never believed possible with 2nd Generation Nintendo Switch. It is designed to adapt wherever you go, whatever you do. Go from single and multi-player games at home to the handheld mode and take it outside with you. Simply attach the Joy-Con controllers to the high definition screen and you’re good to go. Invite your friends over and use two Joy-Con controllers to play face-to-face, or hop online for an epic 4v4 match. Get the gaming system that lets you play the games you want, however you like. "
+    },    {
+        name:"Racing Style Gaming Desk",
+        img:"https://secure.img1-fg.wfcdn.com/im/08631044/resize-h700-w700%5Ecompr-r85/1249/124916251/Racing+Style+Gaming+Desk.jpg",
+        des:"Convenient charging port – Smart USB gaming handle rack with four charging ports, can charge with the phone, Ipad, or headphone."
+    },    {
+        name:"Racing Style Gaming Desk",
+        img:"https://secure.img1-fg.wfcdn.com/im/28134056/resize-h700-w700%5Ecompr-r85/1159/115998574/Racing+Style+Gaming+Desk.jpg",
+        des:"Convenient charging port – Smart USB gaming handle rack with four charging ports, can charge with the phone, Ipad, or headphone."
+    },    {
+        name:"MODEL O-",
+        img:"https://cdn.shopify.com/s/files/1/0549/2681/products/glorious_gaming_mouse_model_o_04_05aff2ee-1b58-45da-b955-533cebe6736a_1000x.png?v=1602782136",
+        des:"Envisioned by a community of passionate gamers, and developed by a team who accepts nothing less than perfection - Model O will elevate your play to unimaginable heights."
+    },    {
+        name:"MODEL O-",
+        img:"https://cdn.shopify.com/s/files/1/0549/2681/products/glorious_gaming_mouse_model_o_17_b44f69ac-481e-49c6-9663-edaceed2c435_1000x.png?v=1602782220",
+        des:"Envisioned by a community of passionate gamers, and developed by a team who accepts nothing less than perfection - Model O will elevate your play to unimaginable heights."
+    },    {
+        name:"MODEL O-",
+        img:"https://cdn.shopify.com/s/files/1/0549/2681/products/1_shopify_34a94812-d4ef-4393-b8f9-c91d6bee62e9_1000x.jpg?v=1588605584",
+        des:"Envisioned by a community of passionate gamers, and developed by a team who accepts nothing less than perfection - Model O will elevate your play to unimaginable heights."
+    },    {
+        name:"Gaming Desk",
+        img:"https://secure.img1-fg.wfcdn.com/im/14511462/resize-h700-w700%5Ecompr-r85/1218/121869161/Gaming+Desk.jpg",
+        des:"Great personal work desk with extra shelves design K generation. The gaming table helps you stand out from competitions e-sports - This table is sleek and stylish perfect for a work desk for business or uses for children desks at home suitable for any environment. Stylish design gives an eclectic feel to any room and decor."
+    },    {
+        name:"CORSAIR Obsidian Series 500D RGB SE Mid Tower Case, Premium Tempered Glass and Aluminum, LL120 Fans and Commander PRO Included, CC-9011139-WW",
+        img:"https://c1.neweggimages.com/ProductImageCompressAll1280/11-139-126-V31.jpg",
+        des:"Iconic Obsidian style, with a beautiful tempered glass front panel and clean exterior lines."
+    },    {
+        name:"Xbox Series X Console",
+        img:"https://cdn.shopify.com/s/files/1/0490/7312/7591/products/gaming-shop-canada-598065_500x.jpg?v=1602274927",
+        des:"The Xbox Series X offers a gaming powerhouse that pairs a custom 1TB SSD and 12 teraflops of graphic processing power for amazingly fast gameplay and reduced load times. Experience 4K gaming at up to 120fps, advanced 3D spatial sound and many new stunning features. The Series X supports thousands of titles across 4 generations of Xbox consoles."
+    },    {
+        name:"Ring Fit Adventure (Switch)",
+        img:"https://cdn.shopify.com/s/files/1/0490/7312/7591/products/gaming-shop-519128_500x.jpg?v=1602274915",
+        des:"Experience a workout like never before with Ring Fit Adventure for Nintendo Switch."
+    },    {
+        name:"PDP LVL40 Gaming Headset for Switch - Blue/Red",
+        img:"https://cdn.shopify.com/s/files/1/0490/7312/7591/products/gaming-shop-352910_500x.jpg?v=1604950925",
+        des:"Immerse yourself in the action of your favourite games with the PDP LVL40 gaming headset for Nintendo Switch. It features a lightweight and breathable design for comfort and powerful audio drivers so you don't miss a sound. The noise-cancelling mic with a flip-to-mute feature keeps you in constant communication with your teammates."
     },
 
 ]
 
-let card = document.querySelector("#card")
+let Ben = [
+    {
+        img: "https://www.thehonestcounter.com/wp-content/uploads/2017/12/avocado.jpg?x29720",
+        name: "Avocado",
+        des: "Lorem ipsum"
+    },
+    {
+        img: "https://th.bing.com/th/id/R76407307c03978838d9ad467e94d0b4f?rik=Sd%2foEoQ1PWbsuw&pid=ImgRaw",
+        name: "Blackberry",
+        des: "Lorem ipsum"
+    },
+    {
+        img: "https://th.bing.com/th/id/Rfd1d300166a7264eedafe79c6c147383?rik=Gvrj6tU3uG6kaw&pid=ImgRaw",
+        name: "Blueberry",
+        des: "Lorem ipsum"
+    },
+    {
+        img: "https://th.bing.com/th/id/Rc36092de1fb13a74839fbf58c243d02d?rik=sYf5OSVISkRrjQ&riu=http%3a%2f%2fwww.freshpoint.com%2fwp-content%2fuploads%2fcommodity-cherry.jpg&ehk=LBnIAYjRkpypMUgmslW8xVQVHyXAofTGcXjl2W8ngSE%3d&risl=&pid=ImgRaw",
+        name: "Cherry",
+        des: "Lorem ipsum"
+    },
+    {
+        img: "https://th.bing.com/th/id/Rcbff8ce1b7a5b2857107dff90181b2cf?rik=oV6%2fqjDRfs3tuw&pid=ImgRaw",
+        name: "Cantaloupe",
+        des: "Lorem ipsum"
+    },
+    {
+        img: "https://th.bing.com/th/id/Rd4b5f8037f9897677b44d10125887182?rik=Mo0xyOtEnGg6hA&riu=http%3a%2f%2fwww.plantgrower.org%2fuploads%2f6%2f5%2f5%2f4%2f65545169%2fsingle-cranberry-plantx750x750_orig.jpg&ehk=bACacpZvHuDAlY%2b3isbqAvBYd2qASoEjWZNPxJygMfE%3d&risl=&pid=ImgRaw",
+        name: "Cranberry",
+        des: "Lorem ipsum"
+    },
+    {
+        img: "https://th.bing.com/th/id/R8f37f6d6ea2d0c4d13aaa5182d2cd4c9?rik=bY430IU5xzrxyw&pid=ImgRaw",
+        name: "Cucumber",
+        des: "Lorem ipsum"
+    },
+    {
+        img: "https://th.bing.com/th/id/OIP.c-5Nd29OMNmPPll-02pa0wHaGd?pid=ImgDet&rs=1",
+        name: "Guava",
+        des: "Lorem ipsum"
+    },
+    {
+        img: "https://th.bing.com/th/id/R5b0581463a572b1f6f3ae96bf0de6efc?rik=caoD9rVQoXa9Pw&riu=http%3a%2f%2fdreamicus.com%2fdata%2fapple%2fapple-04.jpg&ehk=h8ODUMKKdB%2bZGpBr2z9TYP%2bWVyepKbxUMRaq%2bAjiX%2bg%3d&risl=&pid=ImgRaw",
+        name: "Apple",
+        des: "Apple is good!"
+    },
+    {
+        img: "https://th.bing.com/th/id/R460c6ed23709b3ccaa1004b05f562cd8?rik=Thg0%2fW2nmQtIcA&pid=ImgRaw",
+        name: "Grapefruit",
+        des: "Lorem ipsum"
+    },
+    {
+        img: "https://th.bing.com/th/id/R9071e8ce6ae5799d0ea3d9e624d167fd?rik=mFwG8EAAuk1%2fSQ&pid=ImgRaw",
+        name: "Grape",
+        des: "Lorem ipsum"
+    },
+    {
+        img: "https://th.bing.com/th/id/OIP.J713t6KgpMa6j5dXb9AiXQHaHa?pid=ImgDet&rs=1",
+        name: "Banana",
+        des: "Lorem ipsum"
+    },
+    {
+        img: "https://th.bing.com/th/id/Rd034fbf1a8bb2b20109ed523f26aede8?rik=fw3RkaX3DiyReQ&riu=http%3a%2f%2flavite.ca%2fwp-content%2fuploads%2f2018%2f08%2fJackfruit05.jpg&ehk=7I8tZCBzArhFNf4qVE5Np8tlx%2fa%2bUdaFtPvcLvOwi84%3d&risl=&pid=ImgRaw",
+        name: "Jackfurit",
+        des: "Lorem ipsum"
+    },
+    {
+        img: "https://static.onecms.io/wp-content/uploads/sites/35/2019/03/16190037/healthy-kiwi-recipes-ideas.jpg",
+        name: "Kiwi",
+        des: "Lorem ipsum"
+    },
+    {
+        img: "https://th.bing.com/th/id/R7f7aa80a86d503c8a4750d8c809b25d1?rik=MBHRUqtCdi0G1A&riu=http%3a%2f%2fwww.natural-cure.org%2fwp-content%2fuploads%2fwonders-of-lemon.jpg&ehk=fiie8t6T7EZgAF7PH1%2b27zZJgCrIBK89VJ4v3UMp7H4%3d&risl=&pid=ImgRaw",
+        name: "Lemon",
+        des: "Lorem ipsum"
+    },
+    {
+        img: "https://th.bing.com/th/id/OIP.rS2IJDfjmJcN7C574CDk4QHaG0?pid=ImgDet&rs=1",
+        name: "Lime",
+        des: "Lorem ipsum"
+    },
+]
+let allData = Duong.concat(Ben)
 
-for(let i = 0; i <data.length;i++){
-    let html = `<div class="card" style="width: 23%;">
-    <img src="${data[i].img}" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">${data[i].name}</h5>
-      <p class="card-text">${data[i].des}</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div>
-  </div>`
-  card.innerHTML += html
+
+let renderData = (data)=>{
+    let card = document.querySelector("#card")
+    card.innerHTML = ''
+
+    for(let i = 0; i <data.length;i++){
+        let html = `<div class="card" style="width: 23%;">
+        <img src="${data[i].img}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${data[i].name}</h5>
+          <p class="card-text">${data[i].des}</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>`
+      card.innerHTML += html
+    }
 }
+
+renderData(Duong)
+
+
+let fruit = document.getElementById("fruits")
+fruit.addEventListener("click",()=>{
+    renderData(Ben)
+})
+
+let game = document.getElementById("game")
+game.addEventListener("click",()=>{
+    renderData(Duong)
+})
+
+let form = document.getElementById("search")
+form.onsubmit = (e)=>{
+    e.preventDefault()
+    let input = form.inputData.value.toLowerCase()
+    let result = allData.filter((d)=>{
+        return (d.name.toLowerCase()).includes(input)
+    })
+    
+    renderData(result)
+}
+
+
+
+
+
+
+
